dd8b76320ab5a3fd4c3bfbb24f83c13ac33d2eab
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-05 19:52:51 +0000

    Small tidy up

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 const routes = require('./routes')
 
 // Helper method for generating unique ids
@@ -12,10 +11,8 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 app.use(routes)
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
