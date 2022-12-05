4a840a1595a08382549af53242031511df63de9d
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-05 19:48:47 +0000

    POST /api/tips in the right file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router()
 const tipsRoutes = require('./tips')
 
-const { readFromFile } = require('../../helpers/fsUtils');
-
 router.use('/tips', tipsRoutes)
 
 module.exports = router
--- server.js
+++ server.js
@@ -16,28 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(routes)
 
-
-// POST Route for a new UX/UI tip
-app.post('/api/tips', (req, res) => {
-  console.info(`${req.method} request received to add a tip`);
-
-  const { username, topic, tip } = req.body;
-
-  if (req.body) {
-    const newTip = {
-      username,
-      tip,
-      topic,
-      tip_id: uuid(),
-    };
-
-    readAndAppend(newTip, './db/tips.json');
-    res.json(`Tip added successfully 🚀`);
-  } else {
-    res.error('Error in adding tip');
-  }
-});
-
 // GET Route for retrieving all the feedback
 app.get('/api/feedback', (req, res) => {
   console.info(`${req.method} request received for feedback`);
