4ab5fd2a638ade47d911379a291fc65648ad7ad7
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-05 19:52:14 +0000

    POST /api/feedback in the right file

--- server.js
+++ server.js
@@ -16,36 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(routes)
 
-// POST Route for submitting feedback
-app.post('/api/feedback', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to submit feedback`);
-
-  // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
-
-  // If all the required properties are present
-  if (email && feedbackType && feedback) {
-    // Variable for the object we will save
-    const newFeedback = {
-      email,
-      feedbackType,
-      feedback,
-      feedback_id: uuid(),
-    };
-
-    readAndAppend(newFeedback, './db/feedback.json');
-
-    const response = {
-      status: 'success',
-      body: newFeedback,
-    };
-
-    res.json(response);
-  } else {
-    res.json('Error in posting feedback');
-  }
-});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
